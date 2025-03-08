dc04a51fb8ff6e8c75b1a49454f0766a11d551bf
AsherSSB <gromanenghi04@gmail.com>
2025-03-07 20:23:14 -0700

    minor refactor to client

--- client.py
+++ client.py
@@ -9,8 +9,11 @@ IP = "127.0.0.1"
 PORT = 1234
 
 running = True
+my_username = None
+last_input = None
+client_socket = None
 
-def take_input(client_socket, my_username):
+def take_input(my_username):
     global running
     global last_input
     while running:
@@ -29,7 +32,7 @@ def take_input(client_socket, my_username):
             running = False
             return
 
-def receive_messages(client_socket):
+def receive_messages():
     global running
     while running:
         try:
@@ -41,7 +44,7 @@ def receive_messages(client_socket):
             username_length = int(username_header.decode('utf-8').strip())
             username = client_socket.recv(username_length).decode('utf-8')
 
-            if username == "CLOSING":  # very professional, what could go wrong
+            if username == "CLOSING" or username == "BACK":  # very professional, what could go wrong
                 print('Closing connection.')
                 running = False
                 return
@@ -57,55 +60,61 @@ def receive_messages(client_socket):
             running = False
             return
 
-def receive_server_message(client_socket):
+def receive_server_message():
     message_header = client_socket.recv(HEADER_LENGTH)
     message_length = int(message_header.decode('utf-8').strip())
     server_message = client_socket.recv(message_length).decode('utf-8')
     return server_message
 
-def handle_user_state(client_socket):
-    input_thread = threading.Thread(target=take_input, args=(client_socket, my_username))
+def handle_user_state():
+    input_thread = threading.Thread(target=take_input, args=(my_username,))
     input_thread.daemon = True
     input_thread.start()
-    server_message = receive_server_message(client_socket)
+    server_message = receive_server_message()
     while server_message != "200":
         if server_message == "CLOSING":
             print("Closing connection.")
             sys.exit(0)
         print("\n"+server_message) 
-        server_message = receive_server_message(client_socket)
+        server_message = receive_server_message()
     if last_input == "-1":
-        server_message = receive_server_message(client_socket)
+        server_message = receive_server_message()
         print("\n"+server_message)
 
-    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    receive_thread = threading.Thread(target=receive_messages)
     receive_thread.daemon = True
     receive_thread.start()
 
-    while running:
-        input_thread.join(0.1)
-        receive_thread.join(0.1)
+    
+    input_thread.join()
+    receive_thread.join()
 
-def main():
-    global my_username, running
-    print("Type \"CLOSE\" at any point to exit")
+def set_username():
+    global my_username
     my_username = input("Username: ")
     if my_username == "CLOSE":
-        sys.exit(0)
-
-    # TODO: allow user to enter server ip and port
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        client_socket.connect((IP, PORT))
-    except:
-        print("Could not establish connection to server.")
-        sys.exit(0)
-    
+        return
     username = my_username.encode('utf-8')
     username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
     client_socket.send(username_header + username)
+    handle_user_state()
 
-    handle_user_state(client_socket)
+def connect_to_server():
+    global client_socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # TODO: allow user to enter server ip and port
+    try:
+        client_socket.connect((IP, PORT))
+        set_username()
+    except Exception as e:
+        print(e)
+        print("Could not establish connection to server.")
+        return
+
+def main():
+    global running
+    print("Type \"CLOSE\" at any point to exit")
+    connect_to_server()
 
 if __name__ == "__main__":
     main()
